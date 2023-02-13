62572ff45115a85b8dfdf2ba10925ed0ae4f148f
Minchul An <min@minchulan.io>
2023-02-13 01:19:26 -0500

    commit tree 3afd47a8910e458d25de5898cd3bec55afdb396c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+  #delete
+  delete '/reviews/:id' do 
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON 
+    review.to_json
+  end  
+
+  #post 
+  #create
+  
+
 
 end
